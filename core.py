f8c122f18273fbb7f938faa13603e39ea83b7f1d
sam46 <abdlwahdsa@gmail.com>
2021-01-02 20:12:09 -0500

    parse commands

---
+++ core.py
@@ -0,0 +1,65 @@
+import re
+
+
+def create_cluster_helper(cpu, mem, nodes, geoloc, name):
+    pass
+
+
+def create_cluster(tokens):
+    format = 'format:\n\tcreate cluster <cpu> <mem>g <#-of-nodes> <geolocation> <optional-cluster-name>'
+    if len(tokens) not in [4,5]:
+        return format
+    if len(tokens) == 4:
+        tokens.append('cluster-1')
+    
+    cpu, mem, nodes, geo, name = tokens
+    if not cpu.isnumeric():
+        return format
+    cpu = int(cpu)
+    if cpu < 1 or cpu > 8:
+        return 'cpu must between 1-4'
+    
+    if len(mem)<2 or mem[-1] != 'g' or not mem[:-1].isnumeric():
+        return format
+    mem = int(mem[:-1])
+    if mem < 1 or mem > 4:
+        return 'mem must between 1g-4g'
+    
+    if not nodes.isnumeric():
+        return format
+    nodes = int(nodes)
+    if nodes < 1 or nodes > 4:
+        return 'nodes must between 1-4'
+    
+    total_mem = mem*nodes
+    total_cpu = cpu*nodes
+    if total_cpu > 8:
+        return f'{total_cpu} is just too many cpus. no more than 8 total cpus are allowed'
+    if total_mem > 12:
+        return f'{total_mem}g is just too much mem. no more than 12g total mem is allowed'
+
+    geodict = {
+        'ny': 'ny',
+        'sf':'sf', 
+        'london':'london', 
+        'frankfurt':'frankfurt',
+        'singapore':'singapore', 
+        'lon':'london', 
+        'fran':'frankfurt', 
+        'sing':'singapore',
+    }
+    if geo not in list(geodict.keys()):
+        return f'geo location must be one of:\n\t{", ".join(sorted(geodict.keys()))}'
+
+    if len(name) < 3 or len(name) > 25:
+        return 'cluster name must be 3-25 characters long'
+    if not re.match(r'^[\w-]+$', name):
+        return "invalid cluster name. Only alphanumeric, '-', and '_' characters are allowed"
+    
+    return 'creating new k8s cluster...'
+
+def deploy(tokens):
+    return 'deploying tranx...'
+
+def destroy(tokens):
+    return 'cluster destroyed'
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import os
 from dotenv import load_dotenv
 from pyfiglet import figlet_format
-
+from core import *
 
 load_dotenv()
 
@@ -13,9 +13,36 @@ async def on_ready():
     print(figlet_format('TRANXBOT', font='star_war'))
     print('Tranxbot ready to recieve messages...')
 
+def exec_cmd(msg):
+    tokens = list(filter(str.isalnum, msg.split()))
+    if not tokens:
+        return
+    if ' '.join(tokens[:2]) == 'create cluster':
+        return create_cluster(tokens[2:])
+    elif tokens[0] == 'deploy':
+        return deploy(tokens[1:])
+    elif tokens[0] == 'destroy':
+        return destroy(tokens[1:])
+    else:
+        return '''Available commands:
+\tcreate cluster <cpu> <mem>g <#-of-nodes> <geolocation> <optional-cluster-name>
+\tdestroy <optional-cluster-name>
+\tdeploy <branch> <optional-cluster-name>'''
+
+
 @client.event
 async def on_message(message):
+    if not (
+       len(message.mentions) == 1 
+       and message.mentions[0].id == client.user.id
+       and message.channel.name == 'cluster-deploys' 
+    ):
+        return
     print(message.content)
 
+    prefix = f'<@!{client.user.id}>'
+    if message.content.startswith(prefix):
+        resp = exec_cmd(message.content[len(prefix):].strip().lower())
+        await message.channel.send(resp)
 
 client.run(os.getenv('TOKEN'))
